008aa58d11cad8bd021d89411a0be39bcdb3dad7
janhachoi <wnsgk7654@gmail.com>
2022-12-22 19:13:49 +0900

    12-22

--- Assets/script/move.cs
+++ Assets/script/move.cs
@@ -26,6 +26,10 @@ public class move : MonoBehaviour
             cur_time = lerp_time;
         }
 
-        this.transform.position = Vector3.Lerp(start_pos.position, end_pos.position, cur_time /lerp_time);
+        //https://chicounity3d.wordpress.com/2014/05/23/how-to-lerp-like-a-pro/ // 여기에 수식 다나옴 ㅜㅜ 넘 좋앙
+
+        float t = cur_time / lerp_time;
+        t = Mathf.Sin(t * Mathf.PI * 0.5f);
+        this.transform.position = Vector3.Lerp(start_pos.position, end_pos.position, t);
     }
 }
