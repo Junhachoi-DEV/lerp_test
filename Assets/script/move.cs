1f34c7a6c1cd27df533b1fac64c48a440ad3f8d9
janhachoi <wnsgk7654@gmail.com>
2022-12-22 18:53:22 +0900

    12-22

--- Assets/script/move.cs
+++ Assets/script/move.cs
@@ -1,18 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class move : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Transform start_pos;   
+    public Transform end_pos;
     void Start()
     {
-        
+        this.transform.position = start_pos.position;
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
-        
+        this.transform.position = Vector3.Lerp(start_pos.position, end_pos.position, 10 * Time.deltaTime);
     }
 }
