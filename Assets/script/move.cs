abca024871a21dd6f780ff20f209fe971f408fdc
janhachoi <wnsgk7654@gmail.com>
2022-12-22 19:07:38 +0900

    12-22

--- Assets/script/move.cs
+++ Assets/script/move.cs
@@ -7,14 +7,25 @@ public class move : MonoBehaviour
 {
     public Transform start_pos;   
     public Transform end_pos;
+
+    public float lerp_time = 0.5f;
+    float cur_time = 0f;
+
     void Start()
     {
         this.transform.position = start_pos.position;
     }
 
-    
+
     void Update()
     {
-        this.transform.position = Vector3.Lerp(start_pos.position, end_pos.position, 10 * Time.deltaTime);
+        cur_time += Time.deltaTime;
+
+        if(cur_time>= lerp_time)
+        {
+            cur_time = lerp_time;
+        }
+
+        this.transform.position = Vector3.Lerp(start_pos.position, end_pos.position, cur_time /lerp_time);
     }
 }
